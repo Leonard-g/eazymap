20a754a1b98a8f55db11165dfaac1e9f86dd3952
Leonard-g <66696086+Leonard-g@users.noreply.github.com>
2023-03-30 13:15:33 -0400

    Actualizare views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
@@ -9,29 +6,21 @@ from django.db import IntegrityError
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from .models import Task
-
 from .forms import TaskForm
-
-# Create your views here.
-
 
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html', {"form": UserCreationForm})
     else:
-
         if request.POST["password1"] == request.POST["password2"]:
             try:
-                user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"])
+                user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
                 user.save()
                 login(request, user)
                 return redirect('tasks')
             except IntegrityError:
                 return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
-
         return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
-
 
 @login_required
 def tasks(request):
@@ -42,17 +31,14 @@ def tasks(request):
         tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {"tasks": tasks, "search_query": search_query})
 
-
 @login_required
 def tasks_completed(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks.html', {"tasks": tasks})
 
-
 @login_required
 def create_task(request):
     form = TaskForm()
-
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -65,25 +51,21 @@ def create_task(request):
             task = form.save(commit=False)
             task.user = request.user
             task.save()
-
             return redirect('task_list')
 
     context = {'form': form}
     return render(request, 'create_task.html', context)
-
 
 @login_required
 def signout(request):
     logout(request)
     return redirect('home')
 
-
 def signin(request):
     if request.method == 'GET':
         return render(request, 'signin.html', {"form": AuthenticationForm})
     else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
 
