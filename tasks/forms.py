9b142ada8cbc1d2fa73e9852cbc4414a8604461b
Leonard-g <igermanadames@hotmail.com>
2023-03-30 07:22:22 -0400

    nice change

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'No. medidor',
+    }))
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Ejemplo: 18.5167, -70.0167',
+        'rows': 5,
+    }))
+
     class Meta:
         model = Task
-        fields = ['title', 'description', 'important']
+        fields = ('title', 'description', 'completed')
