cc647c47e3ec1e38e808ed60967d37761b9e70ee
Leonard-g <66696086+Leonard-g@users.noreply.github.com>
2023-03-30 09:25:47 -0400

    Actualizar forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,17 +1,7 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Task
 
-class TaskForm(forms.ModelForm):
-    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'No. medidor',
-    }))
-    description = forms.CharField(widget=forms.Textarea(attrs={
-        'class': 'form-control',
-        'placeholder': 'Ejemplo: 18.5167, -70.0167',
-        'rows': 5,
-    }))
-
+class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ('title', 'description', 'completed')
+        fields = ['title', 'description', 'important']
