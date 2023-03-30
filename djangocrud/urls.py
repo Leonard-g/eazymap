9006c4c7ff157a7f5b158ba0be3c70ce40b27857
Leonard-g <66696086+Leonard-g@users.noreply.github.com>
2023-03-30 12:32:03 -0400

    Actualizar urls.py

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('logout/', views.signout, name='logout'),
     path('signin/', views.signin, name='signin'),
     path('create_task/', views.create_task, name='create_task'),
-    path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
-    path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
-    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+    path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('tasks/<int:task_id>/complete/', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete/', views.delete_task, name='delete_task'),
 ]
